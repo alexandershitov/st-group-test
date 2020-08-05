db815d451f6e3eb063d29b477f6f40be5aac1f9b
alexander <alexander@alexander.alexander>
2020-08-05 11:41:35 +0300

    Stub context for client; Clear views

--- server/src/utils/context.ts
+++ server/src/utils/context.ts
@@ -1,9 +1,32 @@
 import { Title } from './title';
 import { Asset } from './asset';
 
+const stubContext = () => JSON.stringify({
+  doctors: [
+    "Школвский Борис Елизарович",
+    "Воронова Тамара Ильевна",
+    "Барсова Лилия Олеговна",
+    "Крючков Валерий Филиппович",
+  ],
+  records: [
+    {
+      patient: "Шитов Александр Игоревич",
+      doctor: "Школвский Борис Елизарович",
+      timestamp: "2020-08-05T09:00:00Z"
+    },
+    {
+      patient: "Петров Евгений Николаевич",
+      doctor: "Барсова Лилия Олеговна",
+      timestamp: "2020-08-05T10:30:00Z",
+      complaints: "Большая очередь"
+    },
+  ]
+})
+
 const call = (req) => ({
   title: Title.call(req),
-  application: Asset.call('application', 'js')
+  application: Asset.call('application', 'js'),
+  context: stubContext()
 });
 
 export const Context = { call } ;
